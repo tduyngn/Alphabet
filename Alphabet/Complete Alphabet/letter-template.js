f6df8aff1224f6a8a9604219f1341d793010424a
tduyngn <68032868+tduyngn@users.noreply.github.com>
2021-12-11 14:54:33 -0500

    return to normal

--- Alphabet/Complete Alphabet/letter-template.js
+++ Alphabet/Complete Alphabet/letter-template.js
@@ -1,5 +1,13 @@
-var pix = document.getElementsByClassName("pixel");
+var pix = document.getElementsByName("pixel");
 
 for (var i = 0; i < pix.length; i++) {
   pix[i].style.animationDelay = Math.ceil(Math.random()*5000)+"ms";
 }
+$(document).ready(function(){
+         
+  for (var i = 0; i < 10; i++) {  
+      var img = "<img src ='Asset 1.svg'/>"; 
+      $("body .dudu1").append(img);
+  }
+
+})
